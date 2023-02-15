227bed30c8c959885807ca7fe89e4fd3ca63cf46
abdelhak.chahid <ch_abdelhak@hotmail.com>
2023-02-15 13:39:53 +0100

    adding GA

--- test_app.py
+++ test_app.py
@@ -4,7 +4,10 @@
 
 import streamlit as st
 import streamlit_authenticator as stauth
-
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
 
 
 option_models = {
@@ -29,6 +32,43 @@ def option_to_model(level_number):
     return e
 
 
+def inject_ga():
+    GA_ID = "google_analytics"
+
+    # Note: Please replace the id from G-XXXXXXXXXX to whatever your
+    # web application's id is. You will find this in your Google Analytics account
+
+    GA_JS = """
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-2X9MVTB4GR"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-2X9MVTB4GR');
+            </script>
+           """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):  # if cannot find tag
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # recover from backup
+        else:
+            shutil.copy(index_path, bck_index)  # keep a backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+
+inject_ga()
+
+
+
 
 
 
@@ -45,11 +85,6 @@ google_analytics_js = """
 </script>
     """
 st.components.v1.html(google_analytics_js)
-
-
-
-
-
 
 
 
