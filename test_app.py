9f6fdbe8c9842be93489582d72f1b030bfc8c998
abdelhak.chahid <ch_abdelhak@hotmail.com>
2023-05-11 18:03:33 +0200

    test

--- test_app.py
+++ test_app.py
@@ -32,40 +32,40 @@ def option_to_model(level_number):
     return e
 
 
-def inject_ga():
-    GA_ID = "google_analytics"
-
-    # Note: Please replace the id from G-FN3J76D6S1 to whatever your
-    # web application's id is. You will find this in your Google Analytics account
-
-    GA_JS = """
-            <!-- Google tag (gtag.js) -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-FN3J76D6S1"></script>
-            <script>
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-FN3J76D6S1');
-            </script>
-           """
-
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):  # if cannot find tag
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  # recover from backup
-        else:
-            shutil.copy(index_path, bck_index)  # keep a backup
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
-
-
-inject_ga()
+# def inject_ga():
+#     GA_ID = "google_analytics"
+#
+#     # Note: Please replace the id from G-FN3J76D6S1 to whatever your
+#     # web application's id is. You will find this in your Google Analytics account
+#
+#     GA_JS = """
+#             <!-- Google tag (gtag.js) -->
+#             <script async src="https://www.googletagmanager.com/gtag/js?id=G-FN3J76D6S1"></script>
+#             <script>
+#               window.dataLayer = window.dataLayer || [];
+#               function gtag(){dataLayer.push(arguments);}
+#               gtag('js', new Date());
+#
+#               gtag('config', 'G-FN3J76D6S1');
+#             </script>
+#            """
+#
+#     # Insert the script in the head tag of the static template inside your virtual
+#     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+#     logging.info(f'editing {index_path}')
+#     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+#     if not soup.find(id=GA_ID):  # if cannot find tag
+#         bck_index = index_path.with_suffix('.bck')
+#         if bck_index.exists():
+#             shutil.copy(bck_index, index_path)  # recover from backup
+#         else:
+#             shutil.copy(index_path, bck_index)  # keep a backup
+#         html = str(soup)
+#         new_html = html.replace('<head>', '<head>\n' + GA_JS)
+#         index_path.write_text(new_html)
+#
+#
+# inject_ga()
 
 
 
